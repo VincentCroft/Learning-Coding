e6dcd024aa6cb8655df26e782741a27f41311154
VincentCroft <78152294+VincentCroft@users.noreply.github.com>
2022-10-19 14:52:17 +0800

    Add files via upload

---
+++ 抛两个骰子算金额.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+int main()
+{
+    int a,b,win,lost,count,money,max;
+    count=0;
+    money=10;
+    srand((int) time(0));
+    while (money>0)
+    {
+        a=rand()%6+1;
+        b=rand()%6+1;
+        if(a+b==7)
+        {   win++;
+            money=money+4;}
+        else
+        {   lost++;
+            money--;}
+        if(money>max)
+        {max=money;}
+        count++;
+        printf("money=%d,此时为%d回合\n",money,count);
+    }
+    printf("回合数为%d, 赢的次数为%d,输的次数为%d，钱最多时为%d\n",count,win,lost,max);
+    system("pause");
+    return 0;
+}
