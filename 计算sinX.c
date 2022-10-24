879486f73a2d3247964a47205c5af669c1f98faf
VincentCroft <78152294+VincentCroft@users.noreply.github.com>
2022-10-24 13:18:59 +0800

    Add files via upload

---
+++ 计算sinX.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n=1,count=1;
+    double x, sum, term;
+    printf("Input x:");
+    scanf("%lf", &x);
+    sum = x; term = x;
+    do {
+        term=-term*x*x/((n+1)*(n+2));
+        sum=sum+term;
+        n=n+2;
+        count++;
+    } while (fabs(term)>=1e-5);
+    printf("sin(x)=%f count=%d\n", sum,count);
+}
+
