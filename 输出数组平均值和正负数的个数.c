4eceb7a89c98dbb2ab8d63c66bb87b527404d668
VincentCroft <78152294+VincentCroft@users.noreply.github.com>
2022-11-25 09:05:25 +0800

    Add files via upload

---
+++ 输出数组平均值和正负数的个数.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a[20],i=0,x,sum=0,count1=0,count2=0;
+    double average;
+    printf("请输入数，输入完请输入0\n");
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        a[i++]=x;
+        scanf("%d",&x);
+    }
+    for (int j = 0; j <i ; ++j)
+    {
+        sum+=a[j];
+        if(a[j]>0)
+            count1++;
+        else
+            count2++;
+    }
+    average=(sum/(double)i);
+    printf("平均值为%lf\n",average);
+    printf("正数有%d个\n",count1);
+    printf("负数有%d个\n",count2);
+}
